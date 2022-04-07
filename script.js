b20a3c1b860198b45522e7d323c8c12884c36cc1
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-04-07 23:27:30 +0300

    added price and price increase. broke price and price increase (kinda works tho)

--- script.js
+++ script.js
@@ -5,11 +5,23 @@ function clickCookie(e) {
 let cookies = document.querySelector(".count-number")
 let cookiesN = 0;
 let cookiesPerClick = 1;
+let shop1Bought = 1;
+let shop1Price = document.querySelector(".price1");
+let shop1PriceTotal = 10;
+let shop1PriceInc = 10;
+let shopButton = document.querySelector("shop-item");
+
 function IncClickPower(){
-    if (cookiesN >= 10) {
-        cookiesPerClick = cookiesPerClick + 1;
-        cookiesN = cookiesN - 10;
+    if (cookiesN >= shop1PriceTotal) {
+        cookiesPerClick = cookiesPerClick + shop1Bought;
+        cookiesN = cookiesN - shop1PriceTotal;
         cookies.textContent = cookiesN;
+        shop1Bought++;
+        shop1PriceTotal = shop1Price + shop1PriceInc;
+        shop1Price.textContent = shop1PriceTotal;
+    } else {
+        shopButton.classList.add("shop-item-unavailable");
+        setTimeout(() => shopButton.classList.remove("shop-item-unavailable"), 500);
     }
     }
 function add(){
