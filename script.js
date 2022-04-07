289a42cad7ee45e6bb255d36be5dc6ded8302f52
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-04-07 14:48:06 +0300

    test push

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function clickCookie(e) {
     e.classList.add("large-cookie");
     setTimeout(() => e.classList.remove("large-cookie"), 50);
 }
+let cookies = document.querySelector(".count-number")
+let cookiesN = 0;
+function add(){
+    cookiesN++;
+    cookies.textContent = cookiesN;
+}
