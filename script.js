e21fded1c13761129a2591fea4d656ad7b2f476f
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-04-08 16:48:22 +0300

    added animation (1)

--- script.js
+++ script.js
@@ -30,6 +30,26 @@ function shopItemUn(e) {
 }
 
 function add() {
-    cookiesN = cookiesN + cookiesPerClick;
-    cookies.textContent = cookiesN;
+    cookiesN = parseInt(cookiesN) + parseInt(cookiesPerClick);
+    setCookiesNum(cookiesN);
 }
+
+function setCookiesNum(num) {
+    cookies.textContent = num;
+}
+
+document.getElementById('save').onclick = function () {
+    localStorage.setItem('cookiesCount', cookiesN);
+    alert('It\'s saved');
+}
+
+document.getElementById('load').onclick = function () {
+    setCookiesNum(localStorage.getItem('cookiesCount'))
+    alert('It\'s loaded');
+}
+
+setInterval(function () {
+    //Todo remove item falling cookie random time change span to button add all the save load values try to add babushka
+    document.getElementsByTagName('article')[0]
+        .insertAdjacentHTML('beforebegin', "<span class='fallingCookie'>Cookie</span>");
+}, 5000);
