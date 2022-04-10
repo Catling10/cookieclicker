3c8fa047c900a67db81adc4cbfc4007c634c41f5
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-04-10 16:20:22 +0300

    fixed save load

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ let shop1PriceInc = 10;
 document.querySelector("#incPower").onclick =
     function () {
         if (cookiesN >= shop1PriceTotal) {
-            cookiesPerClick = cookiesPerClick + shop1Bought;
-            cookiesN = cookiesN - shop1PriceTotal;
+            cookiesPerClick = parseInt(cookiesPerClick) + parseInt(shop1Bought);
+            cookiesN = parseInt(cookiesN) - parseInt(shop1PriceTotal);
             cookies.textContent = cookiesN;
             shop1Bought++;
-            shop1PriceTotal = shop1PriceTotal + shop1PriceInc;
+            shop1PriceTotal = parseInt(shop1PriceTotal) + parseInt(shop1PriceInc);
             shop1Price.textContent = " " + shop1PriceTotal;
         } else {
             shopItemUn(this)
@@ -42,11 +42,21 @@ function setCookiesNum(num) {
 
 document.getElementById('save').onclick = function () {
     localStorage.setItem('cookiesCount', cookiesN);
+    localStorage.setItem('cookiesPerClick', cookiesPerClick);
+    localStorage.setItem('shop1Bought', shop1Bought);
+    localStorage.setItem('shop1Price', shop1Price);
+    localStorage.setItem('shop1PriceTotal', shop1PriceTotal);
+    localStorage.setItem('shop1PriceInc', shop1PriceInc);
     alert('It\'s saved');
 }
 
 document.getElementById('load').onclick = function () {
+    debugger;
     setCookiesNum(localStorage.getItem('cookiesCount'))
+    cookiesPerClick = localStorage.getItem('cookiesPerClick')
+    shop1Bought = localStorage.getItem('shop1Bought')
+    shop1PriceTotal = localStorage.getItem('shop1PriceTotal')
+    shop1PriceInc = localStorage.getItem('shop1PriceInc')
     alert('It\'s loaded');
 }
 
