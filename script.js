d03bc23124953a5be79bdfe5b34e2a1c518b3f5e
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-04-13 12:54:52 +0300

    updated logics and updated save/load

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ document.querySelector(".cookie").onclick =
 //Todo remove item falling cookie random time change span to button
 let cookies = document.querySelector(".count-number")
 let cookiesN = 0;
-let cookiesPerClick = 1;
+/*let cookiesPerClick = 1;
 let shop1Bought = 1;
 let shop1Price = document.querySelector(".price1");
 let shop1PriceTotal = 10;
 let shop1PriceInc = 10;
 let upgr1Price = document.querySelector(".upgrPrice1")
-/*let upgrades.grannies.price = 100;
+let upgrades.grannies.price = 100;
 let grannyAmm = 5;
 let grannyAmmInc = 0;
 let upgrades.grannies.priceInc = 200;
@@ -21,13 +21,13 @@ let upgrades.grannies.interval;*/
 let fallingCookieAmm = 50;
 document.querySelector("#incPower").onclick =
     function () {
-        if (cookiesN >= shop1PriceTotal) {
-            cookiesPerClick = parseInt(cookiesPerClick) + parseInt(shop1Bought);
-            cookiesN = parseInt(cookiesN) - parseInt(shop1PriceTotal);
+        if (cookiesN >= up.click.priceTotal) {
+            up.click.perClick = parseInt(up.click.perClick) + parseInt(up.click.bought);
+            cookiesN = parseInt(cookiesN) - parseInt(up.click.priceTotal);
             cookies.textContent = cookiesN;
-            shop1Bought++;
-            shop1PriceTotal = parseInt(shop1PriceTotal) + parseInt(shop1PriceInc);
-            shop1Price.textContent = " " + shop1PriceTotal;
+            up.click.bought++;
+            up.click.priceTotal = parseInt(up.click.priceTotal) + parseInt(up.click.priceInc);
+            up.click.price.textContent = " " + up.click.priceTotal;
         } else {
             shopItemUn(this)
         }
@@ -46,6 +46,13 @@ let up = {
         priceInc: 200,
         total: 0,
         amountInc: 5
+    },
+    click: {
+        perClick: 1,
+        bought: 1,
+        price: document.querySelector(".price1"),
+        priceTotal: 10,
+        priceInc: 10
     }
 }
 
@@ -74,7 +81,7 @@ function shopItemUn(e) {
 }
 
 function add() {
-        cookiesN = parseInt(cookiesN) + parseInt(cookiesPerClick);
+        cookiesN = parseInt(cookiesN) + parseInt(up.click.perClick);
         setCookiesNum(cookiesN);
 }
 
@@ -89,22 +96,32 @@ function setCookiesNum(num) {
 
 document.getElementById('save').onclick = function () {
     localStorage.setItem('cookiesCount', cookiesN);
-    localStorage.setItem('cookiesPerClick', cookiesPerClick);
-    localStorage.setItem('shop1Bought', shop1Bought);
-    localStorage.setItem('shop1Price', shop1Price);
-    localStorage.setItem('shop1PriceTotal', shop1PriceTotal);
-    localStorage.setItem('shop1PriceInc', shop1PriceInc);
+    localStorage.setItem('up.click.perClick', up.click.perClick);
+    localStorage.setItem('up.click.bought', up.click.bought);
+    localStorage.setItem('up.click.priceTotal', up.click.priceTotal);
+    localStorage.setItem('up.click.priceInc', up.click.priceInc);
+    localStorage.setItem('up.gr.price', up.gr.price);
+    localStorage.setItem('up.gr.priceInc', up.gr.priceInc);
+    localStorage.setItem('up.gr.total', up.gr.total);
+    localStorage.setItem('up.gr.amountInc', up.gr.amountInc);
+
+
     alert('It\'s saved');
 }
 
 document.getElementById('load').onclick = function () {
     setCookiesNum(localStorage.getItem('cookiesCount'))
     cookiesN = localStorage.getItem('cookiesCount')
-    cookiesPerClick = localStorage.getItem('cookiesPerClick')
-    shop1Bought = localStorage.getItem('shop1Bought')
-    shop1PriceTotal = localStorage.getItem('shop1PriceTotal')
-    shop1PriceInc = localStorage.getItem('shop1PriceInc')
-    shop1Price.textContent = (" "+shop1PriceTotal)
+    up.click.perClick = localStorage.getItem('up.click.perClick')
+    up.click.bought = localStorage.getItem('up.click.bought')
+    up.click.priceTotal = localStorage.getItem('up.click.priceTotal')
+    up.click.priceInc = localStorage.getItem('up.click.priceInc')
+    up.click.price.textContent = (" "+up.click.priceTotal)
+    up.gr.price = localStorage.getItem('up.gr.price')
+    up.gr.priceInc = localStorage.getItem('up.gr.priceInc')
+    up.gr.total = localStorage.getItem('up.gr.total')
+    up.gr.amountInc = localStorage.getItem('up.gr.amountInc')
+    up.gr.priceField.textContent = (" "+up.gr.price)
     alert('It\'s loaded');
 }
 
