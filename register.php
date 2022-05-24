5fd4f1ae49192b78eabf35c598391ab8841ac6a6
Catling10 <99316554+Catling10@users.noreply.github.com>
2022-05-24 17:55:29 +0300

    stole a bunch of not working code. tried to adapt

--- register.php
+++ register.php
@@ -40,3 +40,10 @@ $err = [];
         }
     }
 }
+?>
+
+<form method="POST">
+    Логин <input name="login" type="text" required><br>
+Пароль <input name="password" type="password" required><br>
+<input name="submit" type="submit" value="Зарегистрироваться">
+</form>
